9118fc5580c74fdf2e6e60d8f72a1335c46d255c
Patrik W <166713313+PatrikWennerkivst@users.noreply.github.com>
2025-03-26 10:17:07 +0100

    Lag till att Description kommer fram när man hoover över ett Card

--- src/Card.jsx
+++ src/Card.jsx
@@ -2,19 +2,35 @@ import React from "react";
 
 function Card({product}) {
     return(
-        <div className="card h-100">
+        <div className="card h-100 position-relative">
             <div className="card-img-top d-flex justify-content-center align-items-center p-3 bg-light" style={{ height: "200px" }}>
                 <img
                     src={product.image}
                     alt={product.title}
                     style={{ maxHeight: "100%", maxWidth: "100%", objectFit: "contain" }}
                 />
+            
+
+            
+            <div className="layer">
+                <h3>
+                    {product.title}
+                </h3>
+                <p>
+                    {product.description}
+                </p>
+                <button className="btn btn-primary mt-auto">
+                    Buy Now
+                </button>
             </div>
+        </div>
+
             <div className="card-body d-flex flex-column">
                 <h5 className="card-title" style={{ overflow: "hidden", textOverflow: "ellipsis", display: "-webkit-box", WebkitLineClamp: 2, WebkitBoxOrient: "vertical" }}>
                     {product.title}
                 </h5>
                 <p className="card-text text-success fw-bold">${product.price}</p>
+                
                 <button className="btn btn-primary mt-auto">
                     Buy Now
                 </button>
