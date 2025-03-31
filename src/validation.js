4a1f94891d5f39a2f72d2424826b1e2e852219d5
Jesper Lindberg <JesperLindberg92@protonmail.com>
2025-03-31 21:51:06 +0200

    form validation

--- src/validation.js
+++ src/validation.js
@@ -1,11 +1,11 @@
 function validateFields(fields) {
     const validationRules = {
-        name: { regex: "^[a-zA-Z ,.'-]{2,50}$", errorMessage: "Name is invalid" },
-        email: { regex: "^(?=.{1,50}$)[a-zA-Z0-9._%+-]+@[a-zA-Z0-9]+\.[a-zA-Z]+$", errorMessage: "Email is invalid" },
-        phoneNumber: { regex: "^[0-9 +\-()]{6,50}$", errorMessage: "Phone number is invalid" },
-        streetAdress: { regex: "^[a-zA-Z ,.'-0-9]{2,50}$", errorMessage: "Street adress is invalid" },
-        zipCode: { regex: "^[0-9]{5}$", errorMessage: "Zip code is invalid" },
-        city: { regex: "^[a-zA-Z ,.'-]{2,50}$", errorMessage: "City is invalid" }
+        name: { regex: /^[a-zA-Z ,.'-]{2,50}$/, errorMessage: "Name is invalid" },
+        email: { regex: /^(?=.{1,50}$)[a-zA-Z0-9._%+-]+@[a-zA-Z0-9]+\.[a-zA-Z]+$/, errorMessage: "Email is invalid" },
+        phoneNumber: { regex: /^[0-9 +\-()]{6,50}$/, errorMessage: "Phone number is invalid" },
+        streetAdress: { regex: /^[a-zA-Z0-9 ,.'-]{2,50}$/, errorMessage: "Street address is invalid" },
+        zipCode: { regex: /^[0-9]{5}$/, errorMessage: "Zip code is invalid" },
+        city: { regex: /^[a-zA-Z ,.'-]{2,50}$/, errorMessage: "City is invalid" }
     };
 
     let errors = [];
@@ -21,3 +21,29 @@ function validateFields(fields) {
 
     return errors.length > 0 ? errors : null;
 }
+
+document.getElementById("checkout-form").addEventListener("submit", function (e) {
+    event.preventDefault();
+
+    const formData = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        phoneNumber: document.getElementById("phone").value,
+        streetAdress: document.getElementById("street_address").value,
+        zipCode: document.getElementById("postal_code").value,
+        city: document.getElementById("city").value
+    };
+
+    const errors = validateFields(formData);
+
+    if (errors) {
+        errors.forEach(error => {
+            const errorElement = document.getElementById(error.field);
+            if (errorElement) {
+                errorElement.textContent = error.message;
+            }
+        });
+    } else {
+        console.log("Form submitted successfully!");
+    }
+})
